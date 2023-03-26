59e643248798ea510aee365454113150768d83a2
jonkeyguan <jonkey.guan@gmail.com>
2023-03-26 17:02:09 +0800

    added host name param

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ DOMAIN=$(oc cluster-info | grep Kubernetes | awk -F "api." '{print $2}' | awk -F
 
 NS=$(oc project | awk -F "\"" '{print $2}')
 
-oc new-app -f gitea-persistent-template.yaml --param=GITEA_VERSION=1.19
+oc new-app -f gitea-persistent-template.yaml --param=GITEA_VERSION=1.19 --param=HOSTNAME=gitea-${NS}.apps.${DOMAIN}
